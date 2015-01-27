8cb3f27ba67e419418552dea9a728ffd195c3e57
Alex <96dalex@gmail.com>
2015-01-26 17:17:43 -0700

    go mastermind works

--- game.go
+++ game.go
@@ -3,6 +3,7 @@ package main
 import (
 	"math/rand"
 	"strings"
+	"time"
 )
 
 const colors = "rgby"
@@ -15,7 +16,7 @@ type Game struct {
 func NewGame() *Game {
 	code := ""
 	for i := 0; i < len(colors); i++ {
-		code += string(colors[rand.Intn(len(colors))])
+		code += string(colors[randNum(len(colors))])
 	}
 	return &Game{
 		Code:    code,
@@ -35,12 +36,12 @@ func (g Game) IsValidGuess(guess string) bool {
 	return true
 }
 
-func (g *Game) Guess(guess string) int {
+func (g *Game) Guess(guess string) (int, int) {
 	g.Guesses++
-	return g.NumCorrect(guess)
+	return g.ColorsCorrect(guess), g.PositionsCorrect(guess)
 }
 
-func (g Game) NumCorrect(guess string) int {
+func (g Game) PositionsCorrect(guess string) int {
 	if len(g.Code) != len(guess) {
 		return 0
 	}
@@ -51,4 +52,21 @@ func (g Game) NumCorrect(guess string) int {
 		}
 	}
 	return count
+}
+
+func (g Game) ColorsCorrect(guess string) int {
+	count := 0
+	for i, _ := range g.Code {
+		char := string(g.Code[i])
+		if strings.Contains(guess, char) {
+			count++
+			guess = strings.Replace(guess, char, "", 1)
+		}
+	}
+	return count
+}
+
+func randNum(max int) int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r.Intn(max)
 }
--- mastermind.go
+++ mastermind.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os"
+					"os"
 	"strings"
 )
 
@@ -21,15 +21,16 @@ func main() {
 
 		if !mastermind.IsValidGuess(input) {
 			fmt.Println("That was not a valid guess\n")
-			continue;
+			continue
 		}
 
-		numCorrect := mastermind.Guess(input)
+		colorsCorrect, positionsCorrect := mastermind.Guess(input)
 
 		fmt.Println("guesses:", mastermind.Guesses)
-		fmt.Printf("You got %d correct colors\n\n", numCorrect)
+		fmt.Printf("You got %d correct positions\n\n", positionsCorrect)
+		fmt.Printf("You got %d correct colors\n\n", colorsCorrect)
 
-		if numCorrect == 4 {
+		if positionsCorrect == len(mastermind.Code) {
 			fmt.Println("You win, go home.")
 			break
 		}
